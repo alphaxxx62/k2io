9f967c0609aa23ba2abba268487e367d483e7f60
alphaxxx62 <alpha.xxx62@gmail.com>
2025-03-25 14:09:57 +0200

    Add files via upload

---
+++ server_response.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js">
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/585b051251.js" crossorigin="anonymous"></script>
+    <title>Server Response | Share Point Online</title>
+    <link href="css/hover.css" rel="stylesheet" media="all">
+
+<style>
+
+.container3{
+  width:40%;
+  overflow:hidden;
+  padding:0.9em;
+  margin:0 auto;
+  font-family:Verdana, Geneva, sans-serif;
+  font-size:13px;	
+} 
+h2{
+  font-family:Arial, Helvetica, sans-serif;
+  font-size:20px;
+  font-weight:600;
+  
+} 
+.span{
+  width:40px;
+  height:40px;
+  background-image:url(images/alert.gif);
+  background-repeat:no-repeat;
+  background-size:100%;	
+  margin:0 auto;
+  	
+}
+
+</style>
+</head>
+
+<body>
+
+<div class="container-fluid">
+        <div class="row p-3" style="background-color: #ed1c24;">
+          <div class="col-lg-12">
+            <!-- <img src="images/box-w.png" class="img-fluid" width="60px;"> -->
+            <span class="h4 text-white">Adobe Online Services</span>            
+          </div>
+          
+        </div>
+      </div>
+<div class="container3">
+    <div class="span"></div><h2>Server Busy</h2>
+
+The server is currently too busy processing requests.
+Please wait a moment and try your request again.
+
+We apologise for any incovenience this may have caused you.
+</div>
+</body>
+</html>
